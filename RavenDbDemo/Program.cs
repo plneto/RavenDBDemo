d530468454eee2db95dfccb07b12d5b4a9f2447c
Paulo Neto <paulo.neto@farfetch.com>
2019-09-02 12:12:49 +0100

    first commit

---
+++ RavenDbDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RavenDbDemo
+{
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
